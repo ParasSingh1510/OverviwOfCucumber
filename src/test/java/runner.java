7df90aa8e7db436ba64f667b73b6df58bbaf648a
ParasSingh1510 <133941645+ParasSingh1510@users.noreply.github.com>
2023-05-22 10:15:53 +0530

    first commit

---
+++ src/test/java/runner.java
@@ -0,0 +1,19 @@
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		// path of feature file
+		features = "/Users/parassingh/Assignments/OverviewOfCucumber/src/main/java/Features/assignment.feature",
+		// path of step definition file
+		glue = "Binding", tags = "@tag3", monochrome = true, plugin = { "pretty", "html: target/cucumber-reports" }
+
+)
+
+public class runner extends AbstractTestNGCucumberTests {
+
+}
